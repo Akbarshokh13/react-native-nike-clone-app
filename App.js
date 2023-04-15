627160ba7bbc550de7049ff8fc2f5c3a2dd65e45
Akbarshokh <bumur@DESKTOP>
2023-04-15 16:08:11 +0300

    product screen

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import ProductsScreen from './src/screens/ProductsScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, FlatList, Image } from "react-native";
+import ProductsScreen from "./src/screens/ProductsScreen";
+import ProductDetailsScreen from "./src/screens/ProductDetailsScreen";
+import products from "./src/data/products";
 
 export default function App() {
   return (
     <View style={styles.container}>
-    <ProductsScreen /> 
+      {/* <ProductsScreen />  */}
+      <ProductDetailsScreen />
 
-    <FlatList
-    data={products}
-    renderItem={({ item }) => (
-      <View style={styles.ItemContainer}>
-      <Image 
-      source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png'}}
-      style={styles.image}
+      <FlatList
+        data={products}
+        renderItem={({ item }) => (
+          <View style={styles.ItemContainer}>
+            <Image
+              source={{
+                uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png",
+              }}
+              style={styles.image}
+            />
+          </View>
+        )}
+        numColumns={2}
       />
-      </View>
-    )}
-    numColumns={2}
-    />
 
       <StatusBar style="auto" />
     </View>
@@ -28,8 +33,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    backgroundColor: "#fff",
+  },
 });
